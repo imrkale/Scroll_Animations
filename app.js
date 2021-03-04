33d16e739caabfc721f4683100de756c64dde4e9
Raj Kale <kale44772@gmail.com>
2021-03-04 20:17:21 +0530

    Javascript added

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 new fullpage("#fullpage",{
     autoScrolling:true,
-    navigation:true    
+    navigation:true,
+    onLeave:(origin,destination,direction)=>{
+        const section=destination.item;
+        // console.log(destination.index)
+        const title=section.querySelector('h1')
+        const t1=new TimelineMax({delay:0.5});
+        t1.fromTo(title,0.5,{y:50,opacity:0},{y:0,opacity:1})
+
+        if(destination.index===1)
+        {
+            const chairs=document.querySelector('.chair')
+            const destination=document.querySelector('.description')
+            console.log(chairs[0])
+            t1.fromTo(chairs,0.7,{x:'100%'},{x:'-10%'})
+        }
+    }  
 })
